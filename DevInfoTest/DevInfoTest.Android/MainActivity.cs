fd9f0eb7765842d4b5fce51e5861c2f96a3f8594
dronishe <dronishe@gmail.com>
2019-01-31 14:40:25 +0300

    Initial commit

---
+++ DevInfoTest/DevInfoTest.Android/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace DevInfoTest.Droid
+{
+    [Activity(Label = "DevInfoTest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
